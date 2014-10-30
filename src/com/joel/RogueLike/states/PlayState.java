31f5810a3fa7daeed12c914819114311dce92f50
Joel <posiden104@gmail.com>
2014-10-30 17:40:01 -0400

    The play state, left out of the previous commit

---
+++ src/com/joel/RogueLike/states/PlayState.java
@@ -0,0 +1,27 @@
+package com.joel.RogueLike.states;
+
+import com.joel.RogueLike.handlers.GameStateManager;
+
+public class PlayState extends GameState {
+
+	public PlayState(GameStateManager gsm) {
+		super(gsm);
+	}
+
+	public void update(float dt) {
+
+	}
+
+	public void render() {
+
+	}
+
+	public void handleInput() {
+		
+	}
+
+	public void dispose() {
+		
+	}
+
+}
